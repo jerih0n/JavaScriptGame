d204276e6892dd980dda1d5e8412843d30946fa4
Silent-Era <silentera4@gmail.com>
2016-10-12 18:43:09 +0300

    Adding spikes, still in beta

--- Source code/scripts/spike.js
+++ Source code/scripts/spike.js
@@ -1,59 +1,71 @@
-function drawSpike() {
-    const canvas = document.getElementById("obs");
-    let obstacleCanvas = document.getElementById('c').getContext('2d');
-    let timer = setInterval(animate 20);
-    let direction = false;
-    let currentLocation = {
-        x1:850,
-        y1:419,
-        x2:900,
-        y2:419,
-        x3:875,
-        y3:329
+let ctxObs=document.getElementById("obs").getContext("2d");
+//TODO: Make it a class
+function drawSpikesDown() {
+    //ctxObs.strokeStyle='red';
+    ctxObs.lineWidth=3;
+    ctxObs.shadowBlur=20;
+    let spikeOriginalCoords={
+        x1:909,y1:425,
+        x2:934,y2:425,
+        x3:921.5,y3:325
     };
-    let spike = {
-        function animate() {
-        ctx.clearRect(0, 0, 800, 600);
-
-        ctx.beginPath();
-        ctx.moveTo(location.x1,location.y1);
-        ctx.lineWidth=5;
-        ctx.strokeStyle='green';
-        ctx.shadowBlur=20;
-        ctx.lineTo(location.x2,location.y2);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(location.x2,location.y2);
-        ctx.strokeStyle='green';
-        ctx.lineTo(location.x3,location.y3);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(location.x3,location.y3);
-        ctx.strokeStyle='green';
-        ctx.lineTo(location.x1,location.y1);
-        ctx.stroke();
-        ctx.fill
-
-        if (direction) {
-            spike.x -=5
-            if (spike.x<= 25) {
-                spike.remove();
-            }
-        }
-        requestAnimationFrame(animate);
+    let progress=0;
+    function spikeDrawer(coords,progress) {
+        ctxObs.beginPath();
+        ctxObs.moveTo(coords.x1-progress,coords.y1);
+        ctxObs.lineTo(coords.x2-progress,coords.y2);
+        ctxObs.strokeStyle='red';
+        ctxObs.lineTo(coords.x3-progress,coords.y3);
+        ctxObs.lineTo(coords.x1-progress,coords.y1);
+        ctxObs.stroke();
     }
-    animate()
-    moveLeft : function (location) {
-        xCoordinateMoveSpeed=5;
-        location.x1 -= xCoordinateMoveSpeed;
-        if(location.x1 <1 ) {
-            location.x1 = 0;
-            location.x2=50;
-            location.x3=25;
-            xCoordinateMoveSpeed=0;
-        }
-        location.x2 -= xCoordinateMoveSpeed;
-        location.x3 -= xCoordinateMoveSpeed;
-    },
+    function animate() {
+        window.requestAnimationFrame(function loop(){
+            clearAll();
+            spikeDrawer(spikeOriginalCoords,progress);
+            progress+=2;
+            if(spikeOriginalCoords-progress<=0){
+                return;
+            }
+            window.requestAnimationFrame(loop);
+        })
+    }
+    animate();
 }
+function drawSpikesUp() {
+    //ctxObs.strokeStyle='red';
+    ctxObs.lineWidth=3;
+    ctxObs.shadowBlur=20;
+    let spikeOriginalCoords={
+        x1:909,y1:25,
+        x2:934,y2:25,
+        x3:921.5,y3:125
+    };
+    let progress=0;
+    function spikeDrawer(coords,progress) {
+        ctxObs.beginPath();
+        ctxObs.moveTo(coords.x1-progress,coords.y1);
+        ctxObs.lineTo(coords.x2-progress,coords.y2);
+        ctxObs.strokeStyle='blue';
+        ctxObs.lineTo(coords.x3-progress,coords.y3);
+        ctxObs.lineTo(coords.x1-progress,coords.y1);
+        ctxObs.stroke();
+    }
+    function animate() {
+        window.requestAnimationFrame(function loop(){
+            clearAll();
+            spikeDrawer(spikeOriginalCoords,progress);
+            progress+=2;
+            if(spikeOriginalCoords-progress<=0){
+                return;
+            }
+            window.requestAnimationFrame(loop);
+        })
+    }
+    animate();
+}
+drawSpikesDown();
+drawSpikesUp();
+function clearAll(){
+    ctxObs.clearRect(0,0,900,450);
 }
